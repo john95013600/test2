f0cdc2350ecab3c5aa3afecb96ddecf958dbbdde
Sai Tseng <s101321004@mail1.ncnu.edu.tw>
2016-03-14 00:19:05 +0800

    First commit
    Time to go.

---
+++ hello.js
@@ -0,0 +1,37 @@
+//Construntor
+
+// var box = function(size) {
+// 	this.size = size;
+// 	this.weight = function() {
+// 		return size+"kg";
+// 	};
+// };
+
+// var c = new box(5);
+// console.log(c.weight());
+
+
+//anomonus function
+var mod =(function() {
+	var a= 10 ;
+	var b= 20;
+
+
+	var sf = function(){
+		console.log('runed');
+		a+=b;
+		return a;
+	}
+
+	console.log('run');
+
+	return {
+		sum : sf,
+	}
+})();
+
+var res = mod.sum();
+console.log(res);
+
+var res = mod.sum();
+console.log(res);
